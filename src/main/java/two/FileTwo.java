0ab88981b2577abc86866f64ef97792938a7dac7
Frederic Vidal <frvidal@L540098.SQLI>
2020-11-10 22:18:52 +0100

    2 lines have been added into FileTwo

--- src/main/java/two/FileTwo.java
+++ src/main/java/two/FileTwo.java
@@ -4,6 +4,8 @@ public class FileTwo {
 
     /**
      * Construction
+     * 1 line Added
+     * 1 lines Added
      */
     public FileTwo() {
         System.out.println("File Two is constructed");
