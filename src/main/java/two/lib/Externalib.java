25b9297f58622bee96f2374f8b5ed2cc8fce4318
Frederic Vidal <frvidal@L540098.SQLI>
2020-11-13 06:55:48 +0100

    Adding an external libray of 7 lines

---
+++ src/main/java/two/lib/Externalib.java
@@ -0,0 +1,7 @@
+/**
+ * This is class is a jquery-mock library, an external libray which you be avoided from the audit.
+ * A library present in the project.
+ */
+public class Externalib {
+    
+}
